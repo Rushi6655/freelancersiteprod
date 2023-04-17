0d2bffbcd63189250536ffbc0c7564b1789052c2
rushi <rushishelke2000@gmail.com>
2023-04-16 20:21:24 -0700

    security api setup

---
+++ src/main/java/com/springfreelancerProd/springApi/controller/WelcomeController.java
@@ -0,0 +1,43 @@
+package com.springfreelancerProd.springApi.controller;
+
+import com.springfreelancerProd.springApi.entity.AuthRequest;
+import com.springfreelancerProd.springApi.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to javatechie !!";
+    }
+
+    @PostMapping("/authenticate")
+    public Map<String,String> generateToken(@RequestBody AuthRequest authRequest) {
+        Map<String,String> responce=new HashMap<>();
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+            );
+            responce.put("status", HttpStatus.FOUND.toString());
+            responce.put("tocken",jwtUtil.generateToken(authRequest.getUserName()));
+        } catch (Exception ex) {
+            responce.put("status", HttpStatus.UNAUTHORIZED.toString());
+            responce.put("tocken","");
+        }
+        return responce;
+    }
+}
---
+++ src/main/java/com/springfreelancerProd/springApi/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.springfreelancerProd.springApi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(username.equals("admin")){
+            return new User("admin","pass",new ArrayList<>());
+        }else{
+            return null;
+        }
+    }
+}
