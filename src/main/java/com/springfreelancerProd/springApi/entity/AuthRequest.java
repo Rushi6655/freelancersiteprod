12107b7558ff5c9ada2a21b025ff5bf675b8c433
rushi <rushishelke2000@gmail.com>
2023-04-16 20:51:36 -0700

    lomboc removed

--- src/main/java/com/springfreelancerProd/springApi/entity/AuthRequest.java
+++ src/main/java/com/springfreelancerProd/springApi/entity/AuthRequest.java
@@ -1,14 +1,25 @@
 package com.springfreelancerProd.springApi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthRequest {
 
     private String userName;
     private String password;
+
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
